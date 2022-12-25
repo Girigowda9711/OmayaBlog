3bd583729ace53d6841a0892a07d26ffecba6766
Girigowda9711 <girigowda9711@gmail.com>
2022-12-25 19:40:04 +0530

    fifth and sixth testcase added

---
+++ src/test/java/com/OmayaBlog/Testcases/OmayaBlog3.java
@@ -0,0 +1,50 @@
+package com.OmayaBlog.Testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OmayaBlog3 {
+
+	WebDriver driver;
+	@Test
+	public void fifthtestcase() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://omayo.blogspot.com/");
+		WebElement drop = driver.findElement(By.id("drop1"));
+		Select sel=new Select(drop);
+		sel.selectByVisibleText("doc 2");
+		System.out.println("dropdown is selected");
+		System.out.println("Hey its new sop line added by girigowda");
+		driver.quit();
+		}
+	
+	
+	@Test
+	public void sixtestcase() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://omayo.blogspot.com/");
+		WebElement text = driver.findElement(By.xpath("//input[@name='q' and @type='text' and @class='gsc-input']"));
+		text.sendKeys("Girigowda");
+		System.out.println("fourth test case is passed");
+		System.out.println("its new line added by girigowda");
+		driver.quit();
+		
+	}
+}

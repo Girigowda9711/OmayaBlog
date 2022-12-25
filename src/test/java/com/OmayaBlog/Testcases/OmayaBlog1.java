f85e0a8a1e2d7c1594169148954b815d613d268b
Girigowda9711 <girigowda9711@gmail.com>
2022-12-25 18:50:30 +0530

    first commit

---
+++ src/test/java/com/OmayaBlog/Testcases/OmayaBlog1.java
@@ -0,0 +1,80 @@
+package com.OmayaBlog.Testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OmayaBlog1 {
+
+	WebDriver driver;
+	@Test
+	public void Firsttestcase() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://omayo.blogspot.com/");
+		driver.switchTo().frame("navbar-iframe");
+		WebElement ele = driver.findElement(By.id("b-query"));
+		ele.sendKeys("Girigowda");
+		driver.switchTo().defaultContent();
+		WebElement page = driver.findElement(By.xpath("//a[text()='Page One']"));
+		System.out.println(page);
+		driver.quit();
+
+
+
+	}
+
+	@Test
+	public void secondtestcase() {
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://omayo.blogspot.com/");
+		driver.switchTo().frame("navbar-iframe");
+		WebElement ele = driver.findElement(By.id("b-query"));
+		ele.sendKeys("Girigowda");
+		driver.switchTo().defaultContent();
+		WebElement link = driver.findElement(By.xpath("//a[@id='selenium143']"));
+		link.click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

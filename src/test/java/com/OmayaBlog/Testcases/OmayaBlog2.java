b6748efc545ff85bcd23021dd3ad2c3c928eee55
Girigowda9711 <girigowda9711@gmail.com>
2022-12-25 19:26:12 +0530

    second commit

--- src/test/java/com/OmayaBlog/Testcases/OmayaBlog2.java
+++ src/test/java/com/OmayaBlog/Testcases/OmayaBlog2.java
@@ -6,75 +6,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class OmayaBlog1 {
-
+public class OmayaBlog2 {
+	
 	WebDriver driver;
 	@Test
-	public void Firsttestcase() {
+	public void thirdtestcase() {
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get("https://omayo.blogspot.com/");
-		driver.switchTo().frame("navbar-iframe");
-		WebElement ele = driver.findElement(By.id("b-query"));
-		ele.sendKeys("Girigowda");
-		driver.switchTo().defaultContent();
-		WebElement page = driver.findElement(By.xpath("//a[text()='Page One']"));
-		System.out.println(page);
-		System.out.println("it is successfull");
+		WebElement drop = driver.findElement(By.id("drop1"));
+		Select sel=new Select(drop);
+		sel.selectByVisibleText("doc 2");
+		System.out.println("dropdown is selected");
 		driver.quit();
-
-
-
-	}
-
+		}
+	
+	
 	@Test
-	public void secondtestcase() {
+	public void fourthtestcase() {
+		
 		WebDriverManager.chromedriver().setup();
 		driver=new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		driver.get("https://omayo.blogspot.com/");
-		driver.switchTo().frame("navbar-iframe");
-		WebElement ele = driver.findElement(By.id("b-query"));
-		ele.sendKeys("Girigowda");
-		driver.switchTo().defaultContent();
-		WebElement link = driver.findElement(By.xpath("//a[@id='selenium143']"));
-		link.click();
-		String title = driver.getTitle();
-		System.out.println(title);
+		WebElement text = driver.findElement(By.xpath("//input[@name='q' and @type='text' and @class='gsc-input']"));
+		text.sendKeys("Girigowda");
+		System.out.println("fourth test case is passed");
 		driver.quit();
-
-
-
+		
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+	}
